d0309048cb87531f433b06f23b22bcf94f406003
Barthélémy meynard <barthelemy.meynard@polytechnique.edu>
2018-11-07 20:18:30 +0100

    weight and energy

--- structure_code/statesclass.py
+++ structure_code/statesclass.py
@@ -21,40 +21,41 @@ class States:
         self.Jx = Jx
         self.Jz = Jz
         #self.spins_up = rnd.randint(0, n_spins+1)
-        self.pattern = np.zeros((2*m_trotter,n_spins))
+        self.pattern = np.zeros((2*m_trotter,n_spins,6))
         for i in range (2*m_trotter):
             for j in range(n_spins-1):
-                self.pattern[i,j] = i+j % 2
+                self.pattern[i,j,0] = 1
         
         self.spins_up = 0
     
     
-    def to_boxconfig(self):
-        new_writing = np.zeros((6,2*self.m_trotter))
-        for line in range(2*self.m_trotter):
-            for column in range(self.n_spins-1):
-                if (self.pattern[line,column]!=0):
-                    new_writing[self.pattern[line,column],line]+=1
-                    
-        return new_writing
+#    def to_boxconfig(self):
+#        new_writing = np.zeros((6,2*self.m_trotter))
+#        for line in range(2*self.m_trotter):
+#            for column in range(self.n_spins-1):
+#                if (self.pattern[line,column]!=0):
+#                    new_writing[self.pattern[line,column],line]+=1
+#                    
+#        return new_writing
                     
             
         
     def total_energy(self):
         
-        new_writing = self.to_boxconfig()
-        a = self.dtau*self.Jz/4
-        logco = np.log(np.cosh(self.dtau*self.Jx/2))
-        logsi = np.log(np.sinh(self.dtau*self.Jx/2))
-        energymatrix = np.diag([-a, -a, a*logsi, a*logsi, a*logco, a*logco])
-        energy = np.sum(energymatrix.dot(new_writing))
+        a = self.Jz/4
+        th = self.Jx/2*np.tanh(self.dtau*self.Jx/2)
+        coth = self.Jx/2*np.cotanh(self.dtau*self.Jx/2)
+        energymatrix = np.array([-a, -a, a+coth, a+coth, a+th, a+th])
+        energy = np.sum(self.pattern*energymatrix)
         return energy
     
     def weight(self):
-        new_writing = self.to_boxconfig()
         a = np.exp(self.dtau*self.Jz/4)
         cosh = np.cosh(self.dtau*self.Jx/2)
         sinh = np.sinh(self.dtau*self.Jx/2)
-        weightmatrix = np.diag([1/a, 1/a, -a*sinh, -a*sinh, a*cosh, a*cosh])
-        weight = np.sum(energymatrix.dot(new_writing))
+        weightmatrix = np.array([1/a, 1/a, -a*sinh, -a*sinh, a*cosh, a*cosh])
+        weight = np.prod(self.pattern*weightmatrix)
+        return weight
+    
+    def 
         
