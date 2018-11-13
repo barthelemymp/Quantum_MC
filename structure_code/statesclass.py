69a91d9ca118f555bb1044d2c5edae58ba2fa0c8
Barthélémy meynard <barthelemy.meynard@polytechnique.edu>
2018-11-13 14:46:53 +0100

    return enrgy

--- structure_code/statesclass.py
+++ structure_code/statesclass.py
@@ -15,16 +15,19 @@ class States:
         """Construct a path at inverse temperature beta in dim spatial
            dimensions and ntau imaginary time points"""
 
-        self.m_trotter = m_trotter
+        self.m_trotter = m_trotter #division of the temporary time"
         self.dtau = dtau
         self.n_spins = n_spins
         self.Jx = Jx
         self.Jz = Jz
         #self.spins_up = rnd.randint(0, n_spins+1)
         self.pattern = np.zeros((2*m_trotter,n_spins,6))
+        self.pattern[:,:,:]=np.nan
         for i in range (2*m_trotter):
             for j in range(n_spins):
                 self.pattern[i,j,0] = (i+j+1)%2
+                if (self.pattern[i,j,0] == 0):
+                    self.pattern[i,j,0] = np.nan
         
         self.spins_up = 0
     
@@ -46,7 +49,7 @@ class States:
         th = self.Jx/2*np.tanh(self.dtau*self.Jx/2)
         coth = self.Jx/(2*np.tanh(self.dtau*self.Jx/2))
         energymatrix = np.array([-a, -a, a+coth, a+coth, a+th, a+th])
-        energy = np.sum(self.pattern*energymatrix)
+        energy = np.nansum(self.pattern*energymatrix)
         print("en",energy)
         return energy
     
@@ -55,10 +58,10 @@ class States:
         cosh = np.cosh(self.dtau*self.Jx/2)
         sinh = np.sinh(self.dtau*self.Jx/2)
         weightmatrix = np.array([1/a, 1/a, -a*sinh, -a*sinh, a*cosh, a*cosh])
-        weight = np.prod(self.pattern*weightmatrix)
+        weight = np.nanprod(self.pattern*weightmatrix)
         return weight
     
-    def splitspin(self,pos): #probleme d'origine du dernier mouvement a regler 
+    def splitspin(self,pos,dE): #probleme d'origine du dernier mouvement a regler 
         ae = self.Jz/4
         th = self.Jx/2*np.tanh(self.dtau*self.Jx/2)
         coth = self.Jx/(2*np.tanh(self.dtau*self.Jx/2))
@@ -66,57 +69,76 @@ class States:
         
         print("pos",pos)
         a = np.zeros(6)
+        a[:]=np.nan
         a[0]=1
         b = np.zeros(6)
+        b[:]=np.nan
         b[1]=1
         c = np.zeros(6)
+        c[:]=np.nan
         c[2]=1
         d = np.zeros(6)
+        d[:]=np.nan
         d[3]=1
         e = np.zeros(6)
+        e[:]=np.nan
         e[4]=1
         f = np.zeros(6)
+        f[:]=np.nan
         f[5]=1
-        conf = np.argmax(np.array(self.pattern[pos[0],pos[1],:])) + 1
+        conf = np.nanargmax(np.array(self.pattern[pos[0],pos[1],:])) + 1
         print("conf",conf)
         if(pos[2]==0):
             if(conf==1):
                 self.pattern[pos[0],pos[1],:] = f
-                return np.array([pos[0]+1,pos[1]-1,1,energymatrix[5]-energymatrix[0]])
+                dE += energymatrix[5]-energymatrix[0]
+                return np.array([pos[0]+1,pos[1]-1,1])
+            
             if(conf==2):
                 self.pattern[pos[0],pos[1],:,] = e
-                return np.array([pos[0]+1,pos[1]-1,1,energymatrix[4]-energymatrix[1]])
+                dE +=energymatrix[4]-energymatrix[1]
+                return np.array([pos[0]+1,pos[1]-1,1])
             if(conf==3):
                 self.pattern[pos[0],pos[1],:] = a
-                return np.array([pos[0]+1,pos[1]+1,0,energymatrix[0]-energymatrix[2]])
+                dE += energymatrix[0]-energymatrix[2]
+                return np.array([pos[0]+1,pos[1]+1,0])
             if(conf==4):
                 self.pattern[pos[0],pos[1],:] = b
-                return np.array([pos[0]+1,pos[1]+1,0,energymatrix[1]-energymatrix[3]])
+                dE += energymatrix[1]-energymatrix[3]
+                return np.array([pos[0]+1,pos[1]+1,0])
             if(conf==5):
                 self.pattern[pos[0],pos[1],:] = b
-                return np.array([pos[0]+1,pos[1]-1,1,energymatrix[1]-energymatrix[4]])
+                dE +=energymatrix[1]-energymatrix[4]
+                return np.array([pos[0]+1,pos[1]-1,1])
             if(conf==6):
                 self.pattern[pos[0],pos[1],:] = a
-                return np.array([pos[0]+1,pos[1]-1,1,energymatrix[0]-energymatrix[5]])     
+                dE += energymatrix[0]-energymatrix[5]
+                return np.array([pos[0]+1,pos[1]-1,1])     
         elif(pos[2]==1) :
             if(conf==1):
                 self.pattern[pos[0],pos[1],:] = e
-                return np.array([pos[0]+1,pos[1]+1,0,energymatrix[4]-energymatrix[0]])
+                dE +=energymatrix[4]-energymatrix[0]
+                return np.array([pos[0]+1,pos[1]+1,0])
             if(conf==2):
                 self.pattern[pos[0],pos[1],:] = f
-                return np.array([pos[0]+1,pos[1]+1,0,energymatrix[5]-energymatrix[1]])
+                dE +=energymatrix[5]-energymatrix[1]
+                return np.array([pos[0]+1,pos[1]+1,0])
             if(conf==3):
                 self.pattern[pos[0],pos[1],:] = b
-                return np.array([pos[0]+1,pos[1]-1,1,energymatrix[1]-energymatrix[2]])
+                dE +=energymatrix[1]-energymatrix[2]
+                return np.array([pos[0]+1,pos[1]-1,1])
             if(conf==4):
                 self.pattern[pos[0],pos[1],:] = a
-                return np.array([pos[0]+1,pos[1]-1,1,energymatrix[0]-energymatrix[3]])
+                dE +=energymatrix[0]-energymatrix[3]
+                return np.array([pos[0]+1,pos[1]-1,1])
             if(conf==5):
                 self.pattern[pos[0],pos[1],:] = a
-                return np.array([pos[0]+1,pos[1]+1,0,energymatrix[0]-energymatrix[4]])
+                dE +=energymatrix[0]-energymatrix[4]
+                return np.array([pos[0]+1,pos[1]+1,0])
             if(conf==6):
                 self.pattern[pos[0],pos[1],:] = b
-                return np.array([pos[0]+1,pos[1]+1,0,energymatrix[1]-energymatrix[5]]) 
+                dE +=energymatrix[1]-energymatrix[5]
+                return np.array([pos[0]+1,pos[1]+1,0]) 
         
         
     
@@ -126,8 +148,8 @@ class States:
         print("randspin", n)
         p = [0,n,0]
         for i in range(2*self.m_trotter):
-            p = self.splitspin(p)
-            dE += p[3]
+            p = self.splitspin(p,dE)
+            
             print("p", p)
         return dE
     
@@ -146,6 +168,3 @@ class States:
     
                 
                     
-                
-                
-                
