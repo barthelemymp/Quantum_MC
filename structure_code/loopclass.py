6594b23f4c2edb98e4b8f62ca6a1a01bbcc872af
Etienne Camphuis <etienne.camphuis@polytechnique.edu>
2018-11-28 15:23:35 +0100

    Encoded loopclass. Functions able to create then to detect loop along the graph

--- structure_code/loopclass.py
+++ structure_code/loopclass.py
@@ -15,9 +15,12 @@ class Loop:
         """Construct a path at inverse temperature beta in dim spatial
            dimensions and ntau imaginary time points"""
 
-        self.m_trotter = m_trotter #division of the temporary time"
+        #division of the imagingary time
+        self.m_trotter = m_trotter 
         self.dtau = dtau
+        #number of spins along the chain
         self.n_spins = n_spins
+        #interactions
         self.Jx = Jx
         self.Jz = Jz
         #self.spins_up = rnd.randint(0, n_spins+1)
@@ -28,20 +31,26 @@ class Loop:
                 self.pattern[i,j,0] = (i+j+1)%2
                 if (self.pattern[i,j,0] == 0):
                     self.pattern[i,j,0] = np.nan
+        #graph
+        self.total_graph = np.zeros((2*self.m_trotter, self.n_spins), dtype = int)
+        #spins
+        self.spins = np.zeros((2*m_trotter, n_spins))
         
-        self.spins_up = 0
-
+        #computing the energy depending on the configuration of the white
+        #squares. cf Article
         self.a = self.Jz/4
         self.th = self.Jx/2*np.tanh(self.dtau*self.Jx/2)
         self.coth = self.Jx/(2*np.tanh(self.dtau*self.Jx/2))
         self.energymatrix = np.array([-self.a, -self.a, self.a+self.coth, self.a+self.coth, self.a+self.th, self.a+self.th])
-
+        #computing the weight depending on the configuration of the white
+        #squares. cf Article
         self.b = np.exp(self.dtau*self.Jz/4)
         self.cosh = np.cosh(self.dtau*self.Jx/2)
         self.sinh = np.sinh(self.dtau*self.Jx/2)
         self.weightmatrix = np.array([1/self.b, 1/self.b, -self.b*self.sinh, -self.b*self.sinh, self.b*self.cosh, self.b*self.cosh])
-
-        self.greycase = np.ones((20,20),dtype=np.uint8) * 70
+        
+        #initializing the image
+        self.greycase = np.ones((20,20),dtype=np.uint8) * 130
         self.case1 = np.ones((20,20))*255
         self.case2 = np.ones((20,20))*255
         self.case3 = np.ones((20,20),dtype=np.uint8)*255
@@ -58,7 +67,12 @@ class Loop:
         self.case6[:,:2]=0
         self.case5[:,18:]=0
         self.cases = [self.case1, self.case2, self.case3, self.case4, self.case5, self.case6]
-
+        
+        #computing the weight for the passage from the pattern to the graph
+        #the graph 3 (cf Article, i.e. the graph along witch each spin is flipped)
+        #is NOT allowed
+        #The weight of each graph depending on the previous configuration of
+        #the pattern is computed thanks to equation (45) from the Article
         self.inv = np.array([[ 0.5, -0.5,  0.5], \
                              [ 0.5,  0.5, -0.5], \
                              [-0.5,  0.5,  0.5]])
@@ -69,13 +83,55 @@ class Loop:
         self.w22 = np.dot(self.inv, self.w)[1]
         self.w31 = np.dot(self.inv, self.w)[0]
         self.w34 = np.dot(self.inv, self.w)[2]
-
+        #initilizing the graphs
         self.graph1 = self.case2
         self.graph2 = np.transpose(self.case2)
         self.graph3 = np.ones((20,20))*255
-        self.graph4 = self.case3 + self.case4
-        self.graphs = [self.graph1, self.graph2, self.graph3, self.graph4]
+        self.graph4 = self.case3/2 + self.case4/2
+        self.graphs = [self.graph1, self.graph2, self.graph3, self.graph4, self.greycase]
 
+
+    def total_energy(self):
+        energy = np.nansum(self.pattern*self.energymatrix)
+        return energy
+    
+    def weight(self):
+        weight = np.nanprod(self.pattern*self.weightmatrix)
+        return weight
+
+    
+    def spins_to_pattern(self):
+        for i in range(self.n_spins):
+            for l in range(self.m_trotter):
+                conf = np.zeros(6)
+                conf[:]=np.nan
+                if i % 2:
+                    j = 2 * l + 1
+                else:
+                    j = 2 * l 
+                vertg = np.bool(self.spins[i, j] * self.spins[(i+1)%self.n_spins, j])
+                vertd = np.bool(self.spins[i, (j+1)%(2*self.m_trotter)] * self.spins[(i+1)%self.n_spins, (j+1)%(2*self.m_trotter)])
+                diag1 = np.bool(self.spins[i, j] * self.spins[(i+1)%self.n_spins, (j+1)%(2*self.m_trotter)])
+                diag2 = np.bool(self.spins[i, (j+1)%(2*self.m_trotter)] * self.spins[(i+1)%self.n_spins, j])
+#                print(i, j, vertg, vertd, diag1, diag2)
+                if vertg*vertd:
+                    assert(diag1)
+                    assert(diag2)
+                    conf[1] = 1
+                elif diag1:
+                    conf[2] = 1
+                elif diag2:
+                    conf[3] = 1
+                elif vertg:
+                    conf[5] = 1
+                elif vertd:
+                    conf[4] = 1
+                else:
+                    conf[0] = 1
+                
+                self.pattern[i, j] = conf
+        return
+    
     def createimage(self):    
         fig, ax = plt.subplots(figsize = (10,10))
         image = np.zeros((20*self.m_trotter*2,20*self.n_spins))
@@ -88,23 +144,17 @@ class Loop:
                     image[20*(l-1):20*(l),20*j:20*(j+1)]=self.cases[conf]
                 else:
                     image[20*(l-1):20*(l),20*j:20*(j+1)]=130
-                    
-                
+        
         image = np.array(image,dtype=np.uint8)
-        ax.imshow(image)
+        ax.imshow(image, cmap = "Greys_r")
 
-    def total_energy(self):
-        energy = np.nansum(self.pattern*self.energymatrix)
-        return energy
-    
-    def weight(self):
-        weight = np.nanprod(self.pattern*self.weightmatrix)
-        return weight
-
-
-
-    def white_squareingraph(self, pos):
-        conf = np.nanargmax(np.array(self.pattern[pos[0],pos[1],:]))
+                
+    def white_square_in_graph(self, pos):
+        conf_array = np.array(self.pattern[pos[0],pos[1],:])
+        if not (False in np.isnan(conf_array)):
+            return 5
+        
+        conf = np.nanargmax(conf_array)
 
         #image_conf = self.cases[conf]
 
@@ -129,9 +179,90 @@ class Loop:
             else:
                 graph = 2
 
-        image_graph = self.graphs[graph - 1]
-
-        return image_graph
+        return graph
+    
+    def set_total_graph(self):
+        for i in range(self.n_spins):
+            for j in range(2*self.m_trotter):
+                self.total_graph[i,j] = self.white_square_in_graph(np.array([i, j]))
+        return
+        
+    
+    def loop_from_graph(self, spin, pos_graph):
+        graph = self.total_graph[pos_graph]
+        assert(graph != 5)
+        
+        modulo_trotter = 2*self.m_trotter
+        modulo_spin = self.n_spins
+        
+        spin_i_minus = (spin[0] - 1)%modulo_trotter
+        spin_i = spin[0]
+        spin_i_plus = (spin[0] + 1)%modulo_trotter
+        spin_j_minus = (spin[1] - 1)%modulo_spin
+        spin_j = spin[1]
+        spin_j_plus = (spin[1] + 1)%modulo_spin
+        
+        pos_graph_up = (pos_graph[0] + 1)%modulo_trotter
+        pos_graph_down = (pos_graph[0] - 1)%modulo_trotter
+        pos_graph_left = (pos_graph[1] - 1)%modulo_spin
+        pos_graph_right = (pos_graph[1] + 1)%modulo_spin
+        
+        #where is the spin on the graph ?
+        if spin_i == pos_graph[0]:
+            if spin_j == pos_graph[1]:
+                #spin is bottom left of the graph
+                if graph == 1:
+                    #graph is vertical
+                    return ((spin_i_plus, spin_j), (pos_graph_up, pos_graph_left))
+                elif graph == 2:
+                    #graph is horizontal
+                    return ((spin_i, spin_j_plus), (pos_graph_down, pos_graph_right))
+                elif graph == 4:
+                    #graph is cross
+                    return ((spin_i_plus, spin_j_plus), (pos_graph_up, pos_graph_right))
+            else:
+                #spin is bottom right
+                if graph == 1:
+                    #graph is vertical
+                    return ((spin_i_plus, spin_j), (pos_graph_up, pos_graph_right))
+                elif graph == 2:
+                    #graph is horizontal
+                    return ((spin_i, spin_j_minus), (pos_graph_down, pos_graph_left))
+                elif graph == 4:
+                    #graph is cross
+                    return ((spin_i_plus, spin_j_minus), (pos_graph_up, pos_graph_left))
+        else:
+            if spin[1] == pos_graph[1]:
+                #spin is up left
+                if graph == 1:
+                    #graph is vertical
+                    return ((spin_i_minus, spin_j), (pos_graph_down, pos_graph_left))
+                elif graph == 2:
+                    #graph is horizontal
+                    return ((spin_i, spin_j_plus), (pos_graph_up, pos_graph_right))
+                elif graph == 4:
+                    #graph is cross
+                    return ((spin_i_minus, spin_j_plus), (pos_graph_down, pos_graph_right))
+            else:
+                #spin is up right
+                if graph == 1:
+                    #graph is vertical
+                    return ((spin_i_minus, spin_j), (pos_graph_down, pos_graph_right))
+                elif graph == 2:
+                    #graph is horizontal
+                    return ((spin_i, spin_j_minus), (pos_graph_up, pos_graph_left))
+                elif graph == 4:
+                    #graph is cross
+                    return ((spin_i_minus, spin_j_minus), (pos_graph_down, pos_graph_up))
+        return
+    
+    
+    
+    def spin_flip_along_loop(self, ):
+        return
+        
+    
+    
 
     def creategraph(self):
         fig, ax = plt.subplots(figsize = (10, 10))
@@ -140,11 +271,9 @@ class Loop:
             l = self.m_trotter*2 - i
             
             for j in range(self.n_spins):
-                if((i+j+1)%2):
-                    image[20*(l-1):20*(l),20*j:20*(j+1)]=self.white_squareingraph([i, j])
-                else:
-                    image[20*(l-1):20*(l),20*j:20*(j+1)]=130
+#                print(self.white_square_in_graph([i, j]))
+                image[20*(l-1):20*(l),20*j:20*(j+1)]=self.graphs[self.total_graph[i,j] - 1]
                     
-                
+#        print(image)
         image = np.array(image,dtype=np.uint8)
         ax.imshow(image, cmap = 'Greys_r')
--- structure_code/testloop.py
+++ structure_code/testloop.py
@@ -1,7 +1,9 @@
 import localclass as st
 import loopclass as lp
 import numpy as np
+import matplotlib.pyplot as plt
 
+np.random.seed(8)
 s = st.States(4,1,8,1,1)
 for k in range(1000):
     if k%100 == 0:
@@ -14,4 +16,33 @@ for k in range(1000):
 loop = lp.Loop(4,1,8,1,1)
 loop.pattern = s.pattern
 
+#loop.createimage()
+#loop.creategraph()
+
+#print(loop.pattern)
+
+loop.spins = np.array([[0., 1., 0., 0., 0., 0., 0., 0.],
+                    [0., 1., 0., 0., 0., 0., 0., 0.],
+                    [0., 0., 1., 0., 0., 0., 0., 0.],
+                    [0., 0., 0., 1., 0., 0., 0., 0.],
+                    [0., 0., 0., 1., 0., 0., 0., 0.],
+                    [0., 0., 1., 0., 0., 0., 0., 0.],
+                    [0., 1., 0., 0., 0., 0., 0., 0.],
+                    [0., 1., 0., 0., 0., 0., 0., 0.]])
+loop.spins_to_pattern()
+loop.set_total_graph()
+#loop.createimage()
 loop.creategraph()
+
+res = ((0,1), (0,0))
+c = (7,1)
+x = [0]
+y = [1]
+while res[0] != c:
+    res = loop.loop_from_graph(res[0], res[1])
+    x += [res[0][0]]
+    y += [res[0][1]]
+    print(res)
+
+fig = plt.figure(figsize= (10,10))
+plt.scatter(y, x)
